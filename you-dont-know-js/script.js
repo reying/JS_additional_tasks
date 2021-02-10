7c4573ade3db1854c9008aa41851a4c52121edbf
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-02-10 13:30:35 +0500

    Заменил картинку заднего фона

--- you-dont-know-js/script.js
+++ you-dont-know-js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const collectionBooks = document.querySelectorAll('.book');
-
 const ad = document.querySelector('.adv');
+const collectionBooks = document.querySelectorAll('.book');
+const bgBody = document.querySelector('body');
+
 
 console.log(collectionBooks);
 
@@ -11,3 +12,10 @@ ad.remove();
 collectionBooks[0].before(collectionBooks[1]);
 collectionBooks[3].before(collectionBooks[4]);
 collectionBooks[5].after(collectionBooks[2]);
+
+bgBody.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
+
+console.log();
+
+
+// background-image: url(./image/you-dont-know-js.jpg);
